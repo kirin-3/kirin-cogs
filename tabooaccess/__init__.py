d02e5f61e4a4f8eeb58035f21e450f3a75715cc4
kirin-3 <kirin35@protonmail.com>
2025-05-06 18:12:02 +0300

    fix it!

--- tabooaccess/__init__.py
+++ tabooaccess/__init__.py
@@ -1,6 +1,6 @@
 from .tabooaccess import TabooAccess
 
 async def setup(bot):
-    await bot.add_cog(tabooaccess())
+    await bot.add_cog(TabooAccess())
 
 __red_end_user_data_statement__ = "This cog does not store any end user data."
